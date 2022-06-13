8b8ee6124b7c9fd79afe150594d12d2147f9d5e3
Shengchen Zhang <shengchenzhang1207@gmail.com>
2022-06-14 01:04:31 +0800

    Linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     parser: '@babel/eslint-parser',
   },
   rules: {
+    'indent': ["error", 4],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
