c4b66f3ea8d47232d0c2c94a30f92a0208c8c2d0
Shengchen Zhang <shengchenzhang1207@gmail.com>
2022-07-25 21:36:14 +0800

    Update ipc.js

--- src/background/ipc.js
+++ src/background/ipc.js
@@ -19,6 +19,21 @@ ipcMain.on('show-file', (event, file) => {
 
 ipcMain.handle('get-version', () => app.getVersion());
 
+ipcMain.handle('get-packaged', () => app.isPackaged);
+
+ipcMain.handle('check-open-project', () => {
+    // Handle file opening. Need to be done before app is ready.
+    if (app.isPackaged) {
+        // workaround for missing executable argument)
+        process.argv.unshift(null);
+    }
+    // Parameters are now an array containing any files/folders
+    // that your OS will pass to your application
+    const filesToOpen = process.argv.slice(2);
+    console.log(filesToOpen);
+    return filesToOpen;
+});
+
 ipcMain.handle('new-project', () => {
     const projectPath = dialog.showSaveDialogSync({
         filters: [{ name: 'Snowball Project', extensions: ['snowball'] }],
