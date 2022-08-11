9acb6c50b7b39573d72a5b952f3601ebddc776b4
Shengchen Zhang <shengchenzhang1207@gmail.com>
2022-08-11 23:16:20 +0800

    Enable deleting and adding sheets

--- src/components/papers/Tabs.vue
+++ src/components/papers/Tabs.vue
@@ -6,13 +6,20 @@
         @delete="deleteSheet"
         @change="$store.commit('setActiveSheet', $event)"
     > -->
-    <a-tabs size="medium"
+    <a-tabs size="medium" editable show-add-button
+        @add="newEmptySheet"
+        @delete="deleteSheet"
         :default-active-key="$store.state.activeSheet"
         :active-key="$store.state.activeSheet"
         @change="$store.commit('setActiveSheet', $event)"
     >
-        <a-tab-pane :disabled="$store.getters.currentSheet.papers.length === 0">
-            <template #title><icon-star/>All</template>
+        <a-tab-pane :disabled="$store.getters.currentSheet.papers.length === 0"
+            :closable="false"
+        >
+            <template #title><icon-star/>
+                All
+                ({{Object.keys($store.state.papers).length}})
+            </template>
         </a-tab-pane>
         <a-tab-pane v-for="sheet of $store.state.sheets" :key="sheet.id"
             :closable="sheet.id !== 'core'"
