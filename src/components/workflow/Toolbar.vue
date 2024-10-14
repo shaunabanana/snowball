dab2ca95edeabca81613c1c7358ad395b31bc6d2
Shauna Zhang <shengchenzhang1207@gmail.com>
2024-10-14 01:23:00 +0100

    Update Toolbar.vue

--- src/components/workflow/Toolbar.vue
+++ src/components/workflow/Toolbar.vue
@@ -2,44 +2,78 @@
     <a-space class="toolbar">
         <!-- {{$store.state.workflow}} -->
 
-        <a-button-group size="small">
-            <a-button @click="$emit('create', 'import')">
-                <template #icon><icon-import /></template>
-                Import
-            </a-button>
-            <a-button @click="$emit('create', 'sheet')">
-                <template #icon><icon-plus /></template>
-                Sheet
-            </a-button>
-            <a-button @click="$emit('create', 'export')">
-                <template #icon><icon-export /></template>
-                Export
-            </a-button>
-        </a-button-group>
+        <a-space direction="vertical" size="mini">
+            <div style="color: gray; font-size: 0.8em">
+                Basic functions
+            </div>
+            <a-button-group size="mini">
+                <a-button @click="$emit('create', 'import')">
+                    <template #icon><icon-import /></template>
+                    Import
+                </a-button>
+                <a-button @click="$emit('create', 'sheet')">
+                    <template #icon><icon-plus /></template>
+                    Sheet
+                </a-button>
+                <a-button @click="$emit('create', 'export')">
+                    <template #icon><icon-export /></template>
+                    Export
+                </a-button>
+            </a-button-group>
+        </a-space>
 
-        <a-button-group size="small">
-            <a-button @click="$emit('create', 'merge')">
-                <template #icon><icon-copy /></template>
-                Merge
-            </a-button>
-            <a-button @click="$emit('create', 'filter')">
-                <template #icon><icon-search /></template>
-                Filter
-            </a-button>
-            <a-button @click="$emit('create', 'tag')">
-                <template #icon><icon-tag /></template>
-                Tag
-            </a-button>
-        </a-button-group>
+        <a-space direction="vertical" size="mini">
+            <div style="color: gray; font-size: 0.8em">
+                Paper operations
+            </div>
+            <a-button-group size="mini">
+                <a-button @click="$emit('create', 'merge')">
+                    <template #icon><icon-copy /></template>
+                    Merge
+                </a-button>
+                <a-button @click="$emit('create', 'filter')">
+                    <template #icon><icon-search /></template>
+                    Filter
+                </a-button>
+                <a-button @click="$emit('create', 'tag')">
+                    <template #icon><icon-tag /></template>
+                    Tag
+                </a-button>
+            </a-button-group>
+        </a-space>
 
-        <!-- <a-tooltip content="Coming soon!"> -->
-            <a-button-group size="small">
+        <a-space direction="vertical" size="mini">
+            <div style="color: gray; font-size: 0.8em">
+                Snowballing
+            </div>
+            <a-button-group size="mini">
                 <a-button @click="$emit('create', 'snowball')">
                     <template #icon><icon-play-arrow /></template>
                     Snowball
                 </a-button>
+                <a-button @click="$emit('create', 'graph')" disabled>
+                    <template #icon><icon-relation /></template>
+                    Graph
+                </a-button>
             </a-button-group>
-        <!-- </a-tooltip> -->
+        </a-space>
+
+        <a-space direction="vertical" size="mini">
+            <div style="color: gray; font-size: 0.8em">
+                Other
+            </div>
+            <a-button-group size="mini">
+                <a-button @click="$emit('create', 'boolean')">
+                    <template #icon><icon-interaction /></template>
+                    Boolean
+                </a-button>
+
+                <a-button @click="$emit('create', 'script')">
+                    <template #icon><icon-code /></template>
+                    Script
+                </a-button>
+            </a-button-group>
+        </a-space>
 
         <!-- <a-button-group size="small">
             <a-button @click="$emit('create', 'script')">
